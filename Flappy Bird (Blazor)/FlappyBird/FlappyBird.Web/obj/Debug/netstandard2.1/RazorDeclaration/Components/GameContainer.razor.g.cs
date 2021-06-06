d84ed2e29c2e8a00df6d902635e7164c68374796
AlexanderPanagyurski <alexander.panagyurski@gmail.com>
2021-06-06 14:45:05 +0300

    Add css and images

--- Flappy Bird (Blazor)/FlappyBird/FlappyBird.Web/obj/Debug/netstandard2.1/RazorDeclaration/Components/GameContainer.razor.g.cs
+++ Flappy Bird (Blazor)/FlappyBird/FlappyBird.Web/obj/Debug/netstandard2.1/RazorDeclaration/Components/GameContainer.razor.g.cs
@@ -4,7 +4,7 @@
 #pragma warning disable 0649
 #pragma warning disable 0169
 
-namespace FlappyBird.Web
+namespace FlappyBird.Web.Components
 {
     #line hidden
     using System;
@@ -75,7 +75,7 @@ using FlappyBird.Web.Shared;
 #line default
 #line hidden
 #nullable disable
-    public partial class App : Microsoft.AspNetCore.Components.ComponentBase
+    public partial class GameContainer : Microsoft.AspNetCore.Components.ComponentBase
     {
         #pragma warning disable 1998
         protected override void BuildRenderTree(Microsoft.AspNetCore.Components.Rendering.RenderTreeBuilder __builder)
--- Flappy Bird (Blazor)/FlappyBird/FlappyBird.Web/obj/Debug/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
+++ Flappy Bird (Blazor)/FlappyBird/FlappyBird.Web/obj/Debug/netstandard2.1/RazorDeclaration/Pages/FetchData.razor.g.cs
@@ -13,63 +13,63 @@ namespace FlappyBird.Web.Pages
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Components;
 #nullable restore
-#line 1 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 1 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using System.Net.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 2 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 2 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using System.Net.Http.Json;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 3 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 3 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Forms;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 4 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 4 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Routing;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 5 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 5 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 6 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 6 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using Microsoft.AspNetCore.Components.WebAssembly.Http;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 7 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 7 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using Microsoft.JSInterop;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 8 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 8 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using FlappyBird.Web;
 
 #line default
 #line hidden
 #nullable disable
 #nullable restore
-#line 9 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
+#line 9 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\_Imports.razor"
 using FlappyBird.Web.Shared;
 
 #line default
@@ -84,7 +84,7 @@ using FlappyBird.Web.Shared;
         }
         #pragma warning restore 1998
 #nullable restore
-#line 37 "C:\Users\HP\Desktop\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\Pages\FetchData.razor"
+#line 37 "D:\GitHub Repositories\Flappy-Bird--Blazor-\Flappy Bird (Blazor)\FlappyBird\FlappyBird.Web\Pages\FetchData.razor"
        
     private WeatherForecast[] forecasts;
 
